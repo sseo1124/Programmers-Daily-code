5c5204ca1c2e9c173043af25363a23f612d13d6b
sseo1124 <103694448+sseo1124@users.noreply.github.com>
2023-03-23 10:55:12 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,17 +1,2 @@
 def solution(before, after):
-    before_lst = sorted([char for char in before])
-    after_lst = sorted([char for char in after])
-    
-    before = ''
-    for char in before_lst:
-        before += char
-    
-    after = ''
-    for char in after_lst:
-        after += char
-    
-    if before == after:
-        answer = 1
-    else:
-        answer = 0
-    return answer
+    return 1 if sorted(before) == sorted(after) else 0
